90f24cf67f6bf2ecd6de1f4a636f9cf8d7b1ff8e
geekerstar <247507792@qq.com>
2019-11-18 22:14:53 +0800

    Stream

---
+++ java-tutorial-java8/src/main/java/com/geekerstar/stream/StreamTest6.java
@@ -0,0 +1,36 @@
+package com.geekerstar.stream;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author geekerstar
+ * date: 2019/11/18 21:49
+ * description:
+ */
+public class StreamTest6 {
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.generate(UUID.randomUUID()::toString);
+        stream.findFirst().ifPresent(System.out::println);
+
+        Stream.generate(UUID.randomUUID()::toString).findFirst().ifPresent(System.out::println);
+
+        System.out.println("---------------");
+
+        List<Integer> collect = Stream.iterate(1, item -> item + 2).limit(6).collect(Collectors.toList());
+        System.out.println(collect);
+
+        System.out.println("---------------");
+
+        // 找出流中大于2的元素，然后将每个元素乘以2，然后忽略掉流中的前两个元素，然后再取流中的前两个元素，最后求出流中元素总和
+        Stream<Integer> limit = Stream.iterate(1, item -> item + 2).limit(6);
+        System.out.println(limit.filter(item -> item > 2).mapToInt(item -> item * 2).skip(2).limit(2).sum());
+
+//        limit.filter(item -> item > 2).mapToInt(item -> item * 2).skip(2).limit(2).max().ifPresent(System.out::println);
+
+
+
+    }
+}
---
+++ java-tutorial-java8/src/main/java/com/geekerstar/stream/StreamTest7.java
@@ -0,0 +1,28 @@
+package com.geekerstar.stream;
+
+import java.util.IntSummaryStatistics;
+import java.util.stream.Stream;
+
+/**
+ * @author geekerstar
+ * date: 2019/11/18 22:06
+ * description:
+ */
+public class StreamTest7 {
+    public static void main(String[] args) {
+        Stream<Integer> stream = Stream.iterate(1, item -> item + 2).limit(6);
+        System.out.println("原始数据："+stream);
+        // [1, 3, 5, 7, 9, 11]
+
+        IntSummaryStatistics summaryStatistics = stream.filter(item -> item > 2).
+                mapToInt(item -> item * 2).skip(2).limit(2).summaryStatistics();
+
+        System.out.println(summaryStatistics.getMin());
+        System.out.println(summaryStatistics.getCount());
+        System.out.println(summaryStatistics.getMax());
+
+
+        System.out.println(stream.distinct());
+
+    }
+}
