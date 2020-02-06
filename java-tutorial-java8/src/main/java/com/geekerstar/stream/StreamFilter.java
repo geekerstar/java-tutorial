5ba790aedc6ad5ecb2134a6ece46101ea71ba39e
geekerstar <247507792@qq.com>
2020-02-06 18:55:01 +0800

    up

--- java-tutorial-java8/src/main/java/com/geekerstar/stream/StreamFilter.java
+++ java-tutorial-java8/src/main/java/com/geekerstar/stream/StreamFilter.java
@@ -9,12 +9,16 @@ public class StreamFilter {
     public static void main(String[] args) {
 //        List<Integer> list = Arrays.asList(1,2,3,4,5,6);
         List<Integer> list = Arrays.asList(1,2,3,4,5,6,6,7,7,1);
+
         List<Integer> result = list.stream().filter(i->i%2==0).collect(toList());
         System.out.println(result);
+
         result = list.stream().distinct().collect(toList());
         System.out.println(result);
+
         result = list.stream().skip(5).collect(toList());
         System.out.println(result);
+
         result = list.stream().limit(5).collect(toList());
         System.out.println(result);
 
---
+++ java-tutorial-study/src/main/java/com/geekerstar/test/Test1.java
@@ -0,0 +1,13 @@
+package com.geekerstar.test;
+
+/**
+ * @author geekerstar
+ * date: 2020/1/29 21:02
+ * description:
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        String jack = "jack";
+        int i = 20;
+    }
+}
