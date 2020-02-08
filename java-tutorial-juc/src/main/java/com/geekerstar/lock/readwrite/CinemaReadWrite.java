518645c90e407b2040eb3daaf68eea831257890c
geekerstar <247507792@qq.com>
2020-02-08 11:58:05 +0800

    lock

---
+++ java-tutorial-juc/src/main/java/com/geekerstar/lock/readwrite/CinemaReadWrite.java
@@ -0,0 +1,47 @@
+package com.geekerstar.lock.readwrite;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author geekerstar
+ * @date 2020/2/8 11:21
+ * @description
+ */
+public class CinemaReadWrite {
+    private static ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
+    private static ReentrantReadWriteLock.ReadLock readLock = reentrantReadWriteLock.readLock();
+    private static ReentrantReadWriteLock.WriteLock writeLock = reentrantReadWriteLock.writeLock();
+
+    private static void read() {
+        readLock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "得到了读锁，正在读取");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(Thread.currentThread().getName() + "释放读锁");
+            readLock.unlock();
+        }
+    }
+
+    private static void write() {
+        writeLock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "得到了写锁，正在写入");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(Thread.currentThread().getName() + "释放写锁");
+            writeLock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(()->read(),"Thread1").start();
+        new Thread(()->read(),"Thread2").start();
+        new Thread(()->write(),"Thread3").start();
+        new Thread(()->write(),"Thread4").start();
+    }
+}
