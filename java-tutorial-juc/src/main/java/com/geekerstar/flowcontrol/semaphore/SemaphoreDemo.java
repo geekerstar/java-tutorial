307c76d941b84dad1569522aeee4aafa20371b8c
geekerstar <247507792@qq.com>
2020-02-09 15:31:02 +0800

    并发流控制

---
+++ java-tutorial-juc/src/main/java/com/geekerstar/flowcontrol/semaphore/SemaphoreDemo.java
@@ -0,0 +1,42 @@
+package com.geekerstar.flowcontrol.semaphore;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author geekerstar
+ * @date 2020/2/9 15:30
+ * @description Semaphore用法
+ */
+public class SemaphoreDemo {
+    static Semaphore semaphore = new Semaphore(5, true);
+
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(50);
+        for (int i = 0; i < 100; i++) {
+            service.submit(new Task());
+        }
+        service.shutdown();
+    }
+
+    static class Task implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                semaphore.acquire(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "拿到了许可证");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "释放了许可证");
+            semaphore.release(2);
+        }
+    }
+}
