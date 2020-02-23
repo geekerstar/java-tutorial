f0bd37ac98ef179551e85427ce35f4a591d08326
geekerstar <247507792@qq.com>
2020-02-23 21:19:40 +0800

    线程池测试

--- java-tutorial-juc/src/main/java/com/geekerstar/flowcontrol/countdownlatch/CountDownLatchDemo1.java
+++ java-tutorial-juc/src/main/java/com/geekerstar/flowcontrol/countdownlatch/CountDownLatchDemo1.java
@@ -15,18 +15,14 @@ public class CountDownLatchDemo1 {
         ExecutorService service = Executors.newFixedThreadPool(5);
         for (int i = 0; i < 5; i++) {
             final int no = i + 1;
-            Runnable runnable = new Runnable() {
-
-                @Override
-                public void run() {
-                    try {
-                        Thread.sleep((long) (Math.random() * 10000));
-                        System.out.println("No." + no + "完成了检查。");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } finally {
-                        latch.countDown();
-                    }
+            Runnable runnable = () -> {
+                try {
+                    Thread.sleep((long) (Math.random() * 10000));
+                    System.out.println("No." + no + "完成了检查。");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    latch.countDown();
                 }
             };
             service.submit(runnable);
