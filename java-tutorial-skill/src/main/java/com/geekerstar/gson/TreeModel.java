4546c8410017aa571c36ecc0d5dd1af8137f3791
geekerstar <247507792@qq.com>
2021-08-07 18:13:11 +0800

    uupdate

---
+++ java-tutorial-skill/src/main/java/com/geekerstar/gson/TreeModel.java
@@ -0,0 +1,41 @@
+package com.geekerstar.gson;
+
+
+import com.google.gson.*;
+
+public class TreeModel {
+    private static String jsonString =
+            "{\"name\":\"Mahesh Kumar\", \"age\":21,\"verified\":false,\"marks\": [100,90,85]}";
+
+    public static void main(String[] args) {
+        JsonParser parser = new JsonParser();
+        JsonElement rootNode = parser.parse(jsonString);
+        System.out.println(rootNode);
+        //{"name":"Mahesh Kumar","age":21,"verified":false,"marks":[100,90,85]}
+
+        JsonObject details = rootNode.getAsJsonObject();
+        JsonElement nameNode = details.get("name");
+        System.out.println("name:" + nameNode.getAsString());
+
+        JsonElement ageNode = details.get("age");
+        System.out.println("Age: " + ageNode.getAsInt());
+
+        if (rootNode.isJsonObject()) {
+            details = rootNode.getAsJsonObject();
+            nameNode = details.get("name");
+            System.out.println("Name: " + nameNode.getAsString());
+
+            ageNode = details.get("age");
+            System.out.println("Age: " + ageNode.getAsInt());
+
+            JsonElement verifiedNode = details.get("verified");
+            System.out.println("Verified: " + (verifiedNode.getAsBoolean() ? "Yes" : "No"));
+            JsonArray marks = details.getAsJsonArray("marks");
+
+            for (int i = 0; i < marks.size(); i++) {
+                JsonPrimitive value = marks.get(i).getAsJsonPrimitive();
+                System.out.print(value.getAsInt() + " ");
+            }
+        }
+    }
+}
